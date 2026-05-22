b4ecd53384db53fb78c9b73cd1767cf38d5b3c8d
Ali-Shokry <alishokrytamim2183@gmail.com>
2026-05-22 07:27:31 +0300

    fix: restore missing explore components

---
+++ src/features/explore/translate.ts
@@ -0,0 +1,7 @@
+export const translation = {
+    firstTitle :   "📈 Trending Books This Week",
+    seeAll : "See All",
+    secTitle : "🎯 Active Reading Challenges",
+    thirdTitle : "✨ Popular Quotes This Week",
+    fourthTitle:"📚 Browse by Category",
+};
